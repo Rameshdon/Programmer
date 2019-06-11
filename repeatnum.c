897599d1ece5646977fa95d9f2a5aa6598de58c8
Rameshdon <51689248+Rameshdon@users.noreply.github.com>
2019-06-11 11:44:28 +0530

    Create repeatnum.c

---
+++ repeatnum.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+int num;
+scanf("%d\n",&num);
+int a[10000],mar[100],c=0,no;
+int ind;
+for(ind=1;ind<=num;ind++)
+{
+    scanf("%d",&a[ind]);
+    no=a[ind];
+    mar[no]++;
+}
+for(ind=1;ind<=num;ind++)
+{
+if(mar[ind]>1)
+{
+printf("%d ",ind);
+c++;
+}
+}
+if(c==0)
+{
+    printf("unique");
+}
+}    
+    
+    
