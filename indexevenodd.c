9e83472dabd83219f58455c0f1b559730095e8f8
Rameshdon <51689248+Rameshdon@users.noreply.github.com>
2019-06-11 12:01:56 +0530

    Create indexevenodd.c

---
+++ indexevenodd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+int num;
+scanf("%d\n",&num);
+int a[10000],mar[100],c=0,no;
+int ind;
+for(ind=0;ind<num;ind++)
+{
+    scanf("%d",&a[ind]);
+    no=a[ind];
+    if((no%2==0)&&(ind%2!=0))
+    printf("%d ",no);
+    else if((no%2!=0)&&(ind%2==0))
+    printf("%d ",no);
+}
+}    
+    
+    
