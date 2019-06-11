6ca613fa07fc71e0f81a0b7f3a9af21558b4681b
Rameshdon <51689248+Rameshdon@users.noreply.github.com>
2019-06-11 11:56:54 +0530

    Create indexfirstrepeat.c

---
+++ indexfirstrepeat.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+int num;
+scanf("%d\n",&num);
+int a[10000],mar[100],c=0,no;
+int ind;
+for(ind=0;ind<num;ind++)
+{
+    scanf("%d",&a[ind]);
+    no=a[ind];
+    mar[no]++;
+}
+for(ind=0;ind<num;ind++)
+{
+if(mar[ind]>1)
+{
+printf("%d ",ind);
+c++;
+break;
+}
+}
+if(c==0)
+printf("unique");
+}    
+    
+    
