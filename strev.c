3da9680fb241e742f15a14604a1c2cc10f79f84e
Rameshdon <51689248+Rameshdon@users.noreply.github.com>
2019-06-11 13:14:03 +0530

    Update strev.c

--- strev.c
+++ strev.c
@@ -1,13 +1,32 @@
 #include<stdio.h>
-
+void rev(char* );
 int main()
 {
-char str[1000],temp;
-int i=0,j,len;
+char str[1000];
+int len1,ind,start=0;
 scanf("%[^\n]",str);
-for(len=0;str[len];len++);
-j=len-1;
-while(i<j)
+ for(len1=0;str[len1];len1++);
+ for(ind=0;ind<len1;ind++)
+ {
+ if(str[ind]==32)
+ {
+     str[ind]='\0';
+     rev(str+start);
+     str[ind]=32;
+     start=ind+1;
+ }
+ }
+ rev(str+start);
+printf("%s",str);
+    return 0;
+}  
+void rev(char* str)
+{
+    int i=0,j,len;
+    char temp;
+    for(len=0;str[len];len++);
+    j=len-1;
+    while(i<j)
 {
  temp=str[i];
  str[i]=str[j];
@@ -15,8 +34,5 @@ str[j]=temp;
  i++;
  j--;
 }
-printf("%s",str);
-    return 0;
 }
-    
     
