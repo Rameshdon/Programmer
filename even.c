01fee0a087aafe5925b139e37f76347606cff1ba
Rameshdon <51689248+Rameshdon@users.noreply.github.com>
2019-06-11 10:18:13 +0530

    Update even.c

--- even.c
+++ even.c
@@ -4,11 +4,11 @@ int main()
 {
 long long int num;
 scanf("%lld",&num);
-if(num%2==0&&num>0)
-printf("Even");
-else if(num>0)
-printf("Odd");
-else
+if(num<0)
 printf("invalid");
+else if(num%2==0)
+printf("Even");
+else
+printf("Odd");
 return 0;
 }
